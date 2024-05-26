4d1d196361e16b26d56f146b6f49afa23bb0d952
Xxeno-Sama <xsama35@gmail.com>
2024-05-26 15:21:26 +0530

    Initial done

--- src/app/(main)/_components/Sidebar.tsx
+++ src/app/(main)/_components/Sidebar.tsx
@@ -1,9 +1,18 @@
 import React from "react"
 
 const Sidebar = () => {
-  return <div className="h-full w-full bg-blue-700">
-    
-  </div>
+  return (
+    <div className="h-full w-full bg-blue-700">
+      {[...Array(10)].map((_, i) => (
+        <div
+          key={i}
+          className="mb-2 flex h-12 items-center rounded-lg bg-blue-500 p-4 text-2xl"
+        >
+          Navigation{i + 1}
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Sidebar
